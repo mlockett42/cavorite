e1ca67fd57fcfc5b34c686a9bb2d98253a6fe986
Mark Lockett <mlockett@bigpond.com>
2018-01-02 11:55:10 +0800

    Add missing HTML tags to cavorite

--- cavorite/HTML.py
+++ cavorite/HTML.py
@@ -10,10 +10,28 @@ def ClassFactory(name, BaseClass):
     newclass = type(str(name), (BaseClass,),{"__init__": __init__})
     return newclass
 
-htmltagnames = {'a', 'i', 'li', 'span', 'div', 'center', 'ul', 'nav',
-                'button', 'span', 'form', 'footer', 'small', 'p'}
+htmltagnames = {'a', 'abbr', 'acronym', 'address', 'applet', 'area', 'article',
+                'aside', 'audio', 'b', 'base', 'basefont', 'bdi', 'bdo', 'big',
+                'blockquote', 'body', 'br', 'canvas', 'caption',
+                'center', 'cite', 'code', 'col', 'colgroup', 'datalist', 'dd',
+                'del', 'details', 'dfn', 'dialog', 'dir', 'div', 'dl', 'dt',
+                'em', 'embed', 'fieldset', 'figcaption', 'figure', 'font', 
+                'footer', 'form', 'frame', 'frameset', 'h1', 'h2', 'h3', 'h4',
+                'h5', 'h6', 'head', 'header', 'hr', 'html', 'i', 'iframe', 
+                'img', 'ins', 'kbd', 'label', 'legend', 'li', 'link',
+                'main', 'map', 'mark', 'menu', 'menuitem', 'meta', 'meter', 
+                'nav', 'noframes', 'noscript', 'ol', 'optgroup', 
+                'option', 'output', 'p', 'param', 'picture', 'pre', 'progress',
+                'q', 'rp', 'rt', 'ruby', 's', 'samp', 'script', 'section',
+                'select', 'small', 'source', 'span', 'strike', 'strong',
+                'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td',
+                'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr',
+                'track', 'tt', 'u', 'ul', 'var', 'video', 'wbr'
+                }
 
 for tagname in htmltagnames:
     globals()[tagname] = ClassFactory(tagname, c)
 
-html_button = button
+html_button = ClassFactory('button', c)
+html_input = ClassFactory('input', c)
+html_object = ClassFactory('object', c)
