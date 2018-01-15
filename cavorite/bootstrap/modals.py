d51bd89923b32a141feb51a85c43994160714564
Mark Lockett <mlockett@bigpond.com>
2018-01-15 16:59:22 +0800

    Move bootstrap modal support to cavorite

---
+++ cavorite/bootstrap/modals.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals, print_function
+from ..HTML import *
+import copy
+from .. import get_current_hash
+
+class ModalTrigger(a):
+    def __init__(self, attribs, children, target):
+        attribs = copy.copy(attribs)
+        attribs.update({'data-toggle': "modal", 'data-target': target, 'href': get_current_hash()})
+        super(ModalTrigger, self).__init__(attribs, children)
+
+
+class Modal(div):
+    def __init__(self, id, title, body):
+        self.id = id
+        self.title = title
+        self.body = body
+        super(Modal, self).__init__({'class': "modal fade", "id":id, "tabindex": "-1", "role": "dialog", "aria-labeledby": "{}Label".format(id), "aria-hidden": "true"})
+
+    def get_children(self):
+        return  [
+                  div({'class': "modal-dialog", "role": "document"}, [
+                    div({'class': 'modal-content'}, [
+                      div({'class': 'modal-header'}, [
+                        h5({'class': 'modal-title', 'id':  "{}Label".format(self.id)}, self.title),
+                        html_button({'type': 'button', 'class': 'close', 'data-dismiss': 'modal', 'aria-label': 'Close'}, [
+                          span({'aria-hidden': 'true'}, 'X'),
+                        ]),
+                      ]),
+                      div({'class': 'modal-body'}, [
+                        form(self.body)
+                      ]),
+                      div({'class': 'modal-footer'}, [
+                        html_button({'type': "button", 'class':"btn btn-secondary", 'data-dismiss':"modal"}, 'Cancel'),
+                        html_button({'type': "button", 'class':"btn btn-primary"}, 'OK'),
+                      ]),
+                    ]),
+                  ]),
+                ]
+
+
