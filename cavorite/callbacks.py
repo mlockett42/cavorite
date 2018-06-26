8b82509fbb8c32a9dda33015af838ec272d49ec9
Mark Lockett <mark@deliveryengine.net>
2018-06-26 09:38:49 +0800

    Catch python exceptions inside callbacks, print to the console and rethrow

--- cavorite/callbacks.py
+++ cavorite/callbacks.py
@@ -4,10 +4,13 @@ try:
     import js
 except ImportError:
     js = None
+import sys
+import traceback
+
 
 global_callbacks = None
 
-supported_callback_names = {'onclick', 'onchange', 'oncontextmenu', 
+supported_callback_names = {'onclick', 'onchange', 'oncontextmenu',
                             'ondblclick', 'onmousedown', 'onmouseenter',
                             'onmouseleave', 'onmousemove', 'onmouseover',
                             'onmouseup', 'onsubmit',
@@ -16,6 +19,7 @@ supported_callback_names = {'onclick', 'onchange', 'oncontextmenu',
 global_callback_handlers = { k: None for k in supported_callback_names }
 
 def initialise_global_callbacks():
+    #TODO: These callbacks contain a lot of repeated code. It should go in some functions
     # initialise callbacks for commonly used handlers
     global global_callbacks
     global supported_callback_names
@@ -23,112 +27,200 @@ def initialise_global_callbacks():
 
     @js.Function
     def local_onclick_handler(e):
-        global global_callbacks
-        callbacks = global_callbacks['onclick']
-        target = e.target
-        cavorite_id = str(target.getAttribute('_cavorite_id'))
-        if cavorite_id in callbacks:
-            callbacks[cavorite_id](e)
+        try:
+            global global_callbacks
+            callbacks = global_callbacks['onclick']
+            target = e.target
+            cavorite_id = str(target.getAttribute('_cavorite_id'))
+            if cavorite_id in callbacks:
+                callbacks[cavorite_id](e)
+        except Exception as err:
+            # Print the exception info because by default we don't get that
+            # in pypyjs event handlers
+            #exc_info = sys.exc_info()
+            #(exc_type, exc_value, exc_traceback) = exc_info
+            #print('Inner exception exc_type=',exc_type)
+            #print('Error message')
+            #print(err.message)
+            print("Exception")
+            #print('Inner exception exc_value=',exc_value)
+            #print('Inner exception exc_traceback=',exc_traceback)
+            #print('Inner exception extract_tb=',traceback.extract_tb(exc_traceback))
+            #print('dir(err)',dir(err))
+            #print('exc_traceback=',exc_traceback)
+            #print('err.trace=', err.trace)
+            traceback.print_exc(file=sys.stdout)
+            #del exc_info
+            raise
     global global_callback_handlers
     global_callback_handlers['onclick'] = local_onclick_handler
 
     @js.Function
     def local_onchange_handler(e):
-        global global_callbacks
-        callbacks = global_callbacks['onchange']
-        target = e.target
-        cavorite_id = str(target.getAttribute('_cavorite_id'))
-        if cavorite_id in callbacks:
-            callbacks[cavorite_id](e)
+        try:
+            global global_callbacks
+            callbacks = global_callbacks['onchange']
+            target = e.target
+            cavorite_id = str(target.getAttribute('_cavorite_id'))
+            if cavorite_id in callbacks:
+                callbacks[cavorite_id](e)
+        except Exception as err:
+            # Print the exception info because by default we don't get that
+            # in pypyjs event handlers
+            print("Exception")
+            traceback.print_exc(file=sys.stdout)
+            raise
     global_callback_handlers['onchange'] = local_onchange_handler
 
     @js.Function
     def local_oncontextmenu_handler(e):
-        global global_callbacks
-        callbacks = global_callbacks['oncontextmenu']
-        target = e.target
-        cavorite_id = str(target.getAttribute('_cavorite_id'))
-        if cavorite_id in callbacks:
-            callbacks[cavorite_id](e)
+        try:
+            global global_callbacks
+            callbacks = global_callbacks['oncontextmenu']
+            target = e.target
+            cavorite_id = str(target.getAttribute('_cavorite_id'))
+            if cavorite_id in callbacks:
+                callbacks[cavorite_id](e)
+        except Exception as err:
+            # Print the exception info because by default we don't get that
+            # in pypyjs event handlers
+            print("Exception")
+            traceback.print_exc(file=sys.stdout)
+            raise
     global_callback_handlers['oncontextmenu'] = local_oncontextmenu_handler
 
     @js.Function
     def local_ondblclick_handler(e):
-        global global_callbacks
-        callbacks = global_callbacks['ondblclick']
-        target = e.target
-        cavorite_id = str(target.getAttribute('_cavorite_id'))
-        if cavorite_id in callbacks:
-            callbacks[cavorite_id](e)
+        try:
+            global global_callbacks
+            callbacks = global_callbacks['ondblclick']
+            target = e.target
+            cavorite_id = str(target.getAttribute('_cavorite_id'))
+            if cavorite_id in callbacks:
+                callbacks[cavorite_id](e)
+        except Exception as err:
+            # Print the exception info because by default we don't get that
+            # in pypyjs event handlers
+            print("Exception")
+            traceback.print_exc(file=sys.stdout)
+            raise
     global_callback_handlers['ondblclick'] = local_ondblclick_handler
 
     @js.Function
     def local_onmousedown_handler(e):
-        global global_callbacks
-        callbacks = global_callbacks['onmousedown']
-        target = e.target
-        cavorite_id = str(target.getAttribute('_cavorite_id'))
-        if cavorite_id in callbacks:
-            callbacks[cavorite_id](e)
+        try:
+            global global_callbacks
+            callbacks = global_callbacks['onmousedown']
+            target = e.target
+            cavorite_id = str(target.getAttribute('_cavorite_id'))
+            if cavorite_id in callbacks:
+                callbacks[cavorite_id](e)
+        except Exception as err:
+            # Print the exception info because by default we don't get that
+            # in pypyjs event handlers
+            print("Exception")
+            traceback.print_exc(file=sys.stdout)
+            raise
     global_callback_handlers['onmousedown'] = local_onmousedown_handler
 
     @js.Function
     def local_onmouseenter_handler(e):
-        global global_callbacks
-        callbacks = global_callbacks['onmouseenter']
-        target = e.target
-        cavorite_id = str(target.getAttribute('_cavorite_id'))
-        if cavorite_id in callbacks:
-            callbacks[cavorite_id](e)
+        try:
+            global global_callbacks
+            callbacks = global_callbacks['onmouseenter']
+            target = e.target
+            cavorite_id = str(target.getAttribute('_cavorite_id'))
+            if cavorite_id in callbacks:
+                callbacks[cavorite_id](e)
+        except Exception as err:
+            # Print the exception info because by default we don't get that
+            # in pypyjs event handlers
+            print("Exception")
+            traceback.print_exc(file=sys.stdout)
+            raise
     global_callback_handlers['onmouseenter'] = local_onmouseenter_handler
 
     @js.Function
     def local_onmouseleave_handler(e):
-        global global_callbacks
-        callbacks = global_callbacks['onmouseleave']
-        target = e.target
-        cavorite_id = str(target.getAttribute('_cavorite_id'))
-        if cavorite_id in callbacks:
-            callbacks[cavorite_id](e)
+        try:
+            global global_callbacks
+            callbacks = global_callbacks['onmouseleave']
+            target = e.target
+            cavorite_id = str(target.getAttribute('_cavorite_id'))
+            if cavorite_id in callbacks:
+                callbacks[cavorite_id](e)
+        except Exception as err:
+            # Print the exception info because by default we don't get that
+            # in pypyjs event handlers
+            print("Exception")
+            traceback.print_exc(file=sys.stdout)
+            raise
     global_callback_handlers['onmouseleave'] = local_onmouseleave_handler
 
     @js.Function
     def local_onmousemove_handler(e):
-        global global_callbacks
-        callbacks = global_callbacks['onmousemove']
-        target = e.target
-        cavorite_id = str(target.getAttribute('_cavorite_id'))
-        if cavorite_id in callbacks:
-            callbacks[cavorite_id](e)
+        try:
+            global global_callbacks
+            callbacks = global_callbacks['onmousemove']
+            target = e.target
+            cavorite_id = str(target.getAttribute('_cavorite_id'))
+            if cavorite_id in callbacks:
+                callbacks[cavorite_id](e)
+        except Exception as err:
+            # Print the exception info because by default we don't get that
+            # in pypyjs event handlers
+            print("Exception")
+            traceback.print_exc(file=sys.stdout)
+            raise
     global_callback_handlers['onmousemove'] = local_onmousemove_handler
 
     @js.Function
     def local_onmouseover_handler(e):
-        global global_callbacks
-        callbacks = global_callbacks['onmouseover']
-        target = e.target
-        cavorite_id = str(target.getAttribute('_cavorite_id'))
-        if cavorite_id in callbacks:
-            callbacks[cavorite_id](e)
+        try:
+            global global_callbacks
+            callbacks = global_callbacks['onmouseover']
+            target = e.target
+            cavorite_id = str(target.getAttribute('_cavorite_id'))
+            if cavorite_id in callbacks:
+                callbacks[cavorite_id](e)
+        except Exception as err:
+            # Print the exception info because by default we don't get that
+            # in pypyjs event handlers
+            print("Exception")
+            traceback.print_exc(file=sys.stdout)
+            raise
     global_callback_handlers['onmouseover'] = local_onmouseover_handler
 
     @js.Function
     def local_onmouseup_handler(e):
-        global global_callbacks
-        callbacks = global_callbacks['onmouseup']
-        target = e.target
-        cavorite_id = str(target.getAttribute('_cavorite_id'))
-        if cavorite_id in callbacks:
-            callbacks[cavorite_id](e)
+        try:
+            global global_callbacks
+            callbacks = global_callbacks['onmouseup']
+            target = e.target
+            cavorite_id = str(target.getAttribute('_cavorite_id'))
+            if cavorite_id in callbacks:
+                callbacks[cavorite_id](e)
+        except Exception as err:
+            # Print the exception info because by default we don't get that
+            # in pypyjs event handlers
+            print("Exception")
+            traceback.print_exc(file=sys.stdout)
+            raise
     global_callback_handlers['onmouseup'] = local_onmouseup_handler
 
     @js.Function
     def local_onsubmit_handler(e):
-        global global_callbacks
-        callbacks = global_callbacks['onsubmit']
-        target = e.target
-        cavorite_id = str(target.getAttribute('_cavorite_id'))
-        if cavorite_id in callbacks:
-            callbacks[cavorite_id](e)
+        try:
+            global global_callbacks
+            callbacks = global_callbacks['onsubmit']
+            target = e.target
+            cavorite_id = str(target.getAttribute('_cavorite_id'))
+            if cavorite_id in callbacks:
+                callbacks[cavorite_id](e)
+        except Exception as err:
+            # Print the exception info because by default we don't get that
+            # in pypyjs event handlers
+            print("Exception")
+            traceback.print_exc(file=sys.stdout)
+            raise
     global_callback_handlers['onsubmit'] = local_onsubmit_handler
-
---
+++ tests/test_callbacks.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals, print_function
+from cavorite.cavorite import callbacks
+from mock import Mock
+import tests.fakejs as js
+import pytest
+
+
+class TestCallbackExceptionHandling(object):
+    def performTest(self, handler_name, monkeypatch, capsys):
+        monkeypatch.setattr(callbacks, 'js', js)
+
+        def fail_now(e):
+            assert False
+
+        target = js.createElement('button')
+        target.html_attribs['_cavorite_id'] = '41a2b617-3dfe-4f39-8a14-58bea7459db2'
+
+        callbacks.initialise_global_callbacks()
+        callbacks.global_callbacks['onclick'][
+            '41a2b617-3dfe-4f39-8a14-58bea7459db2'] = fail_now
+
+        with pytest.raises(AssertionError):
+            callbacks.global_callback_handlers['onclick'](Mock(target=target))
+
+        out, err = capsys.readouterr()
+        assert 'AssertionError' in out
+
+    def test_exceptions_in_onclick_callbacks_are_handled(self, monkeypatch, capsys):
+        self.performTest('onclick', monkeypatch, capsys)
+
+    def test_exceptions_in_onchange_callbacks_are_handled(self, monkeypatch, capsys):
+        self.performTest('onchange', monkeypatch, capsys)
+
+    def test_exceptions_in_oncontextmenu_callbacks_are_handled(self, monkeypatch, capsys):
+        self.performTest('oncontextmenu', monkeypatch, capsys)
+
+    def test_exceptions_in_ondblclick_callbacks_are_handled(self, monkeypatch, capsys):
+        self.performTest('ondblclick', monkeypatch, capsys)
+
+    def test_exceptions_in_onmousedown_callbacks_are_handled(self, monkeypatch, capsys):
+        self.performTest('onmousedown', monkeypatch, capsys)
+
+    def test_exceptions_in_onmouseenter_callbacks_are_handled(self, monkeypatch, capsys):
+        self.performTest('onmouseenter', monkeypatch, capsys)
+
+    def test_exceptions_in_onmouseleave_callbacks_are_handled(self, monkeypatch, capsys):
+        self.performTest('onmouseleave', monkeypatch, capsys)
+
+    def test_exceptions_in_onmousemove_callbacks_are_handled(self, monkeypatch, capsys):
+        self.performTest('onmousemove', monkeypatch, capsys)
+
+    def test_exceptions_in_onmouseup_callbacks_are_handled(self, monkeypatch, capsys):
+        self.performTest('onmouseup', monkeypatch, capsys)
+
+    def test_exceptions_in_onsubmit_callbacks_are_handled(self, monkeypatch, capsys):
+        self.performTest('onsubmit', monkeypatch, capsys)
